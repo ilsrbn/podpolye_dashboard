222d1bf68fa1ea8f81e135c0f348aff342bf1d33
illia.serbin <illia.serbin@tangramltd.com>
2023-02-24 20:52:54 +0200

    update router guard

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,8 +1,10 @@
+
 export const canAccess = async () => {
+  const apiUrl = import.meta.env.VITE_API_URL
   const cookie = localStorage.getItem('authBearer')
   if (!cookie) return false
   try {
-    const resp = await fetch('api/auth/profile', {
+    const resp = await fetch(apiUrl + 'api/auth/profile', {
       headers: {
         Authorization: 'Bearer ' + cookie
       }
