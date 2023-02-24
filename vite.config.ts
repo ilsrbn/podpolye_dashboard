4723b01f95a921987f30b47c2871b711490fc852
illia.serbin <illia.serbin@tangramltd.com>
2023-02-24 20:50:13 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://back.podpolye-api.com',
         changeOrigin: true
       }
     }
